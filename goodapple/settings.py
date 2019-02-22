1590740eca53284d22b1cd3bd43d6fd896e42f6b
hayeun7777 <kellyhypark@gmail.com>
2019-02-21 23:07:38 -0800

    style added

--- goodapple/settings.py
+++ goodapple/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'goodapple',
-		# 'USER': 'postgres', # pgadmin only 
-		# 'PASSWORD': 'postgres', # pgadmin only
+		'USER': 'postgres', # pgadmin only 
+		'PASSWORD': 'postgres', # pgadmin only
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
